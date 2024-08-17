6120a5ba6bda99dcff629b991ba66a21780c9be0
orun-sunny <sunny.mukut@gmail.com>
2024-08-17 19:33:35 +0600

    initial push

---
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AppliedJobs = () => {
+  return (
+    <div>
+      <h2>jobs i loved</h2>
+    </div>
+  );
+};
+
+export default AppliedJobs;
--- src/main.jsx
+++ src/main.jsx
@@ -6,15 +6,22 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Root from "./components/Root/Root";
 import Home from "./components/Home/Home";
+import AppliedJobs from "./components/AppliedJobs/AppliedJobs";
+import ErrorPage from "./components/ErrorPage/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/applied",
+        element: <AppliedJobs />,
       },
     ],
   },
